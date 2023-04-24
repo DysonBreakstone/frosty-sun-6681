03ae7e31cf0156642347a3c23fea3fb661ade751
DysonBreakstone <121531791+DysonBreakstone@users.noreply.github.com>
2023-04-24 10:55:43 -0600

    patients index shows patients over the age of 18

---
+++ spec/features/patients/index_spec.rb
@@ -0,0 +1,26 @@
+require 'rails_helper'
+
+RSpec.describe "patients index page", type: :feature do
+  describe 'display' do
+    before do
+      test_data
+    end
+
+    it "shows all patients over 18" do
+      visit patients_path
+
+      expect(page).to have_content("All Patients over the age of 18:")
+
+      pt_over_18 = [@patient_2, @patient_4, @patient_6, @patient_8]
+      pt_not_over_18 = [@patient_1, @patient_3, @patient_5, @patient_7]
+
+      pt_over_18.each do |pt|
+        expect(page).to have_content(pt.name)
+      end
+
+      pt_not_over_18.each do |pt|
+        expect(page).to have_no_content(pt.name)
+      end
+    end
+  end
+end
--- spec/models/patient_spec.rb
+++ spec/models/patient_spec.rb
@@ -6,4 +6,13 @@ RSpec.describe Patient, type: :model do
     it {should have_many(:doctors).through(:doctor_patients)}
   end
 
+  describe 'class methods' do
+    before do
+      test_data
+    end
+
+    it "::over_18" do
+      expect(Patient.over_18).to eq([@patient_2, @patient_4, @patient_6, @patient_8])
+    end
+  end
 end
