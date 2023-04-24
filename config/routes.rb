6ecd0f1f81428acbbcea493fbdcff3f3686e76e1
DysonBreakstone <121531791+DysonBreakstone@users.noreply.github.com>
2023-04-24 10:34:18 -0600

    delete button deletes patients on doctor show page

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  resources :doctors, only: [:show]
+  resources :doctors, only: [:show] do
+    resources :doctor_patients, only: [:destroy]
+  end
   resources :patients, only: [:index]
-  resources :doctor_patients, only: [:destroy]
   resources :hospitals, only: [:show]
 end
